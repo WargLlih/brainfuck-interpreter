7efaa9c6c0b32b24b77d292e2d83672f112d27fa
Daniel Jr <dewaldman@outlook.com>
2024-03-10 16:26:28 +0000

    Created safe checks and allocated memory for data and instroctions

--- brainfuck.c
+++ brainfuck.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INSTRUCTIONS_SIZE (1 << 32) - 1
-#define MAX_DATA_SIZE (1 << 32) - 1
+#define MAX_INSTRUCTIONS_SIZE ((1UL << 32) - 1)
+#define MAX_DATA_SIZE ((1UL << 32) - 1)
 #define HELP_MESSAGE                                                           \
   "Usage:\n\t %s [option] file\nOptions:\n\t --help\tDisplay usage "           \
   "information.\n\t --dbuffer <n>\tCreate a data buffer with a size "          \
-  "of <n> byte(s).\n"
+  "of <n> byte(s); 0 < n <= %lu\n"
 
 typedef uint8_t bool;
 
 // https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
-uint32_t fsize(FILE *file) {
+uint64_t fsize(FILE *file) {
   fseek(file, 0L, SEEK_END);
   return ftell(file);
 }
 
-bool execute_brainfuck();
+bool execute_brainfuck(uint8_t *data_buffer, uint8_t *instructions) {}
 
 int main(int argc, char *argv[]) {
   uint32_t dbuffer_size = 0;
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 
   case 2:
     if (!strncmp(argv[1], "--help", sizeof("--help"))) {
-      fprintf(stdout, HELP_MESSAGE, argv[0]);
+      fprintf(stdout, HELP_MESSAGE, argv[0], MAX_DATA_SIZE);
       return EXIT_SUCCESS;
     }
     goto PANIC;
@@ -54,9 +54,28 @@ int main(int argc, char *argv[]) {
 
   PANIC:
   default:
-    fprintf(stderr, HELP_MESSAGE, argv[0]);
+    fprintf(stderr, HELP_MESSAGE, argv[0], MAX_DATA_SIZE);
     return EXIT_FAILURE;
   }
 
-  printf("dbuffer_size = %u\n", dbuffer_size);
+  uint64_t file_size = fsize(file);
+  if (file_size > MAX_INSTRUCTIONS_SIZE) {
+    fprintf(stderr, "max supported file size is %lu bytes\n",
+            MAX_INSTRUCTIONS_SIZE);
+    return EXIT_FAILURE;
+  }
+
+  uint8_t *instructions = (uint8_t *)malloc(file_size);
+  if (instructions == NULL) {
+    fprintf(stderr, "Failed to allocate %lu bytes\n", file_size);
+    return EXIT_FAILURE;
+  }
+
+  uint8_t *data_buffer = (uint8_t *)malloc(dbuffer_size);
+  if (data_buffer == NULL) {
+    fprintf(stderr, "Failed to allocate %u bytes\n", dbuffer_size);
+    return EXIT_FAILURE;
+  }
+
+  fgets((char *)instructions, file_size, file);
 };
